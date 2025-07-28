ddb5f920e9a3f9b31b82a6861b3a9f9a011a010c
Charles Wang <charlestfeg@gmail.com>
2025-07-28 11:35:30 -0400

    added basic styling, continue to add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { Profiler } from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className ="flex-div">
+      <div className="nav-left flex-div">
+        <img className="menu-icon" src={assets.menu} alt="" />
+        <img className="logo" src={assets.logo} alt="" />
+      </div>
 
-    </div>
+      <div className="nav-middle flex-div">
+        <input type="text" placeholder='Search' />
+        <img src={assets.search} alt="" />
+      </div>
+
+      <div className="nav-right flex-div">
+        <img src={assets.upload} alt="" />
+        <img src={assets.more} alt="" />
+        <img src={assets.notification} alt="" />
+        <img src={assets.jack} alt="" />
+      </div>
+
+      
+
+    </nav >
   )
 }
 
