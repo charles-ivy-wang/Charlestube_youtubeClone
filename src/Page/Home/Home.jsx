e550d9a117fac82a4ee9de8954f0b04f40eea9b4
Charles Wang <charlestfeg@gmail.com>
2025-08-03 13:51:28 -0400

    started API implementation, need to finish category

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import {React, useState} from "react";
 import "./Home.css";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Feed from "../../components/Feed/Feed";
 import { assets } from "../../assets/assets";
 
-
 const Home = ({ sidebar }) => {
+  
+  {
+    /* change home page content based on categories*/
+  }
+  const [category, setCategory] = useState(0);
+
   return (
     <>
-      <Sidebar sidebar={sidebar} />
+      <Sidebar
+        sidebar={sidebar}
+        category={category}
+        setCategory={setCategory}
+      />
       <div className={`container ${sidebar ? "" : "large-container"}`}>
-        <Feed />
+        <Feed category={category} />
       </div>
     </>
   );
---
+++ src/data.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'AIzaSyDsSC8-NLPFAKyHZx7xxxhN02yC99JuvHg';
+
